b85f36ac5cf2e09b3517c9d0c41f299fe414ff80
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-12-15 18:44:46 +0530

    Create inOrderTraversal.java

---
+++ inOrderTraversal.java
@@ -0,0 +1,39 @@
+public class file6 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=left;
+            this.right=right;
+        }
+    }
+    static class BinaryTree{
+        static int index=-1;
+        static Node buildTree(int[] nodes){
+            index++;
+            if(nodes[index]==-1){
+                return null;
+            }
+            Node newNode=new Node(nodes[index]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+            return newNode;
+        }
+        static void inOrder(Node root){
+            if(root==null){
+                return;
+            }
+            inOrder(root.left);
+            System.out.println(root.data+" ");
+            inOrder(root.right);
+        }
+    }
+    public static void main(String[] args) {
+        int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree=new BinaryTree();
+        Node root=tree.buildTree(nodes);
+        tree.inOrder(root);
+    }
+}
